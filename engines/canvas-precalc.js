22887a26cc7c478d085b456ce2fa977103888e0b
Simo Kinnunen <sorccu@gmail.com>
2008-07-13 16:05:57 +0300

    - precalc before cleanup

--- engines/canvas-precalc.js
+++ engines/canvas-precalc.js
@@ -1,65 +1,65 @@
 Cufon.registerEngine('canvas-precalc', (function() {
 
 	function generate(path) {
-		var code = [];
 		var at = { x: 0, y: 0 }, cp = { x: 0, y: 0 };
 		var cmds = Cufon.SVG.parsePath(path);
+		var code = new Array(cmds.length);
 		for (var i = 0, l = cmds.length; i < l; ++i) {
 			var cmd = cmds[i].type;
 			if (cmd == 'z' || cmd == 'Z') {
-				code.push({ m: 'closePath' });
+				code[i] = { m: 'closePath' };
 				continue;
 			}
 			var c = cmds[i].coords;
 			switch (cmd) {
 				case 'M':
-					code.push({ m: 'moveTo', a: [ at.x = Number(c[0]), at.y = Number(c[1]) ] });
+					code[i] = { m: 'moveTo', a: [ at.x = Number(c[0]), at.y = Number(c[1]) ] };
 					break;
 				case 'L':
-					code.push({ m: 'lineTo', a: [ at.x = Number(c[0]), at.y = Number(c[1]) ] });
+					code[i] = { m: 'lineTo', a: [ at.x = Number(c[0]), at.y = Number(c[1]) ] };
 					break;
 				case 'l':
-					code.push({ m: 'lineTo', a: [ at.x += Number(c[0]), at.y += Number(c[1]) ] });
+					code[i] = { m: 'lineTo', a: [ at.x += Number(c[0]), at.y += Number(c[1]) ] };
 					break;
 				case 'H':
-					code.push({ m: 'lineTo', a: [ at.x = Number(c[0]), at.y ] });
+					code[i] = { m: 'lineTo', a: [ at.x = Number(c[0]), at.y ] };
 					break;
 				case 'h':
-					code.push({ m: 'lineTo', a: [ at.x += Number(c[0]), at.y ] });
+					code[i] = { m: 'lineTo', a: [ at.x += Number(c[0]), at.y ] };
 					break;
 				case 'V':
-					code.push({ m: 'lineTo', a: [ at.x, at.y = Number(c[0]) ] });
+					code[i] = { m: 'lineTo', a: [ at.x, at.y = Number(c[0]) ] };
 					break;
 				case 'v':
-					code.push({ m: 'lineTo', a: [ at.x, at.y += Number(c[0]) ] });
+					code[i] = { m: 'lineTo', a: [ at.x, at.y += Number(c[0]) ] };
 					break;
 				case 'C':
-					code.push({ m: 'bezierCurveTo', a: [ Number(c[0]), Number(c[1]), cp.x = Number(c[2]), cp.y = Number(c[3]), at.x = Number(c[4]), at.y = Number(c[5]) ] });
+					code[i] = { m: 'bezierCurveTo', a: [ Number(c[0]), Number(c[1]), cp.x = Number(c[2]), cp.y = Number(c[3]), at.x = Number(c[4]), at.y = Number(c[5]) ] };
 					break;
 				case 'c':
-					code.push({ m: 'bezierCurveTo', a: [ at.x + Number(c[0]), at.y + Number(c[1]), cp.x = at.x + Number(c[2]), cp.y = at.y + Number(c[3]), at.x += Number(c[4]), at.y += Number(c[5]) ] });
+					code[i] = { m: 'bezierCurveTo', a: [ at.x + Number(c[0]), at.y + Number(c[1]), cp.x = at.x + Number(c[2]), cp.y = at.y + Number(c[3]), at.x += Number(c[4]), at.y += Number(c[5]) ] };
 					break;
 				case 'S':
 					if (i == 0 || !/^[CcSs]$/.test(cmds[i - 1].type)) cp.x = at.x, cp.y = at.y;
-					code.push({ m: 'bezierCurveTo', a: [ at.x + (at.x - cp.x), at.y + (at.y - cp.y), cp.x = Number(c[0]), cp.y = Number(c[1]), at.x = Number(c[2]), at.y = Number(c[3]) ] });
+					code[i] = { m: 'bezierCurveTo', a: [ at.x + (at.x - cp.x), at.y + (at.y - cp.y), cp.x = Number(c[0]), cp.y = Number(c[1]), at.x = Number(c[2]), at.y = Number(c[3]) ] };
 					break;
 				case 's':
 					if (i == 0 || !/^[CcSs]$/.test(cmds[i - 1].type)) cp.x = at.x, cp.y = at.y;
-					code.push({ m: 'bezierCurveTo', a: [ at.x + (at.x - cp.x), at.y + (at.y - cp.y), cp.x = at.x + Number(c[0]), cp.y = at.y + Number(c[1]), at.x += Number(c[2]), at.y += Number(c[3]) ] });
+					code[i] = { m: 'bezierCurveTo', a: [ at.x + (at.x - cp.x), at.y + (at.y - cp.y), cp.x = at.x + Number(c[0]), cp.y = at.y + Number(c[1]), at.x += Number(c[2]), at.y += Number(c[3]) ] };
 					break;
 				case 'Q':
-					code.push({ m: 'quadraticCurveTo', a: [ cp.x = Number(c[0]), cp.y = Number(c[1]), at.x = Number(c[2]), at.y = Number(c[3]) ] });
+					code[i] = { m: 'quadraticCurveTo', a: [ cp.x = Number(c[0]), cp.y = Number(c[1]), at.x = Number(c[2]), at.y = Number(c[3]) ] };
 					break;
 				case 'q':
-					code.push({ m: 'quadraticCurveTo', a: [ cp.x = at.x + Number(c[0]), cp.y = at.y + Number(c[1]), at.x += Number(c[2]), at.y += Number(c[3]) ] });
+					code[i] = { m: 'quadraticCurveTo', a: [ cp.x = at.x + Number(c[0]), cp.y = at.y + Number(c[1]), at.x += Number(c[2]), at.y += Number(c[3]) ] };
 					break;
 				case 'T':
 					if (i == 0 || !/^[QqTt]$/.test(cmds[i - 1].type)) cp.x = at.x, cp.y = at.y;
-					code.push({ m: 'quadraticCurveTo', a: [ cp.x = at.x + (at.x - cp.x), cp.y = at.y + (at.y - cp.y), at.x = Number(c[0]), at.y = Number(c[1]) ] });
+					code[i] = { m: 'quadraticCurveTo', a: [ cp.x = at.x + (at.x - cp.x), cp.y = at.y + (at.y - cp.y), at.x = Number(c[0]), at.y = Number(c[1]) ] };
 					break;
 				case 't':
 					if (i == 0 || !/^[QqTt]$/.test(cmds[i - 1].type)) cp.x = at.x, cp.y = at.y;
-					code.push({ m: 'quadraticCurveTo', a: [ cp.x = at.x + (at.x - cp.x), cp.y = at.y + (at.y - cp.y), at.x += Number(c[0]), at.y += Number(c[1]) ] });
+					code[i] = { m: 'quadraticCurveTo', a: [ cp.x = at.x + (at.x - cp.x), cp.y = at.y + (at.y - cp.y), at.x += Number(c[0]), at.y += Number(c[1]) ] };
 					break;
 				case 'A':
 				case 'a':
@@ -129,7 +129,7 @@ Cufon.registerEngine('canvas-precalc', (function() {
 		
 		if (options.textDecoration) textDecoration: for (var search = node, decoStyle = style; search.parentNode && search.parentNode.nodeType == 1; ) {
 		
-			var line = function(y, color, invert) {
+			function line(y, color, invert) {
 				g.strokeStyle = color;
 				
 				g.beginPath();
@@ -177,9 +177,8 @@ Cufon.registerEngine('canvas-precalc', (function() {
 		g.fillStyle = g.strokeStyle = style.get('color');
 		
 		for (var j = 0, k = chars.length; j < k; ++j) {
-			var char = chars[j];
-			if (!font.glyphs[char] && !font.missingGlyph) continue;
-			var glyph = font.glyphs[char] || font.missingGlyph;
+			var glyph = font.glyphs[chars[j]] || font.missingGlyph;
+			if (!glyph) continue;
 			g.beginPath();
 			if (glyph.d) {
 				if (!glyph.code) glyph.code = generate(glyph.d);
@@ -188,7 +187,7 @@ Cufon.registerEngine('canvas-precalc', (function() {
 			g.fill();
 			g.translate(Number(glyph.h || font.h) + letterSpacing, 0);
 		}
-					
+		
 		for (var fn; fn = buffer.shift(); fn());
 		
 		return canvas;
