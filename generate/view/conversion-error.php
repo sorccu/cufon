a496a289a58dc7994788b4e0046bc481698b2f70
Simo Kinnunen <simo@shoqolate.com>
2009-03-05 13:59:50 +0200

    The error message for failed conversions now includes a link to the "Trouble with font files" Wiki page.

--- generate/view/conversion-error.php
+++ generate/view/conversion-error.php
@@ -51,7 +51,9 @@
 					
 						<h3>Sorry!</h3>
 						
-						<p>The file you uploaded could not be converted. Currently only TrueType (TTF), OpenType (OTF) and Printer Font Binary (PFB) fonts are supported.</p>
+						<p>The file you uploaded could not be converted. Currently only TrueType (TTF), OpenType (OTF), Printer Font Binary (PFB) and PostScript fonts are supported.</p>
+						
+						<p>Some files (especially old Mac PostScript fonts) require pre-processing that cannot be done on the server. For more information please read <a href="http://wiki.github.com/sorccu/cufon/trouble-with-font-files" class="external">Trouble with font files</a>.</p>
 						
 						<p class="info"><strong>Tip:</strong> did you upload an AFM or a PFM file? They only contain font metrics information which is not quite
 						what we need. There should be a PFA or a PFB file with the same name in the same folder, which contains the actual glyph data. If there isn't, your font manager
