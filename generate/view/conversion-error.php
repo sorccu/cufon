4d0e1d0fb9bac30411d987d65f515d2027925cce
Simo Kinnunen <simo@shoqolate.com>
2009-03-06 02:48:26 +0200

    The error message for failed conversions is now even more informative. Added a mention of author-restricted fonts.

--- generate/view/conversion-error.php
+++ generate/view/conversion-error.php
@@ -53,6 +53,8 @@
 						
 						<p>The file you uploaded could not be converted. Currently only TrueType (TTF), OpenType (OTF), Printer Font Binary (PFB) and PostScript fonts are supported.</p>
 						
+						<p>If you're sure the font is valid, it is likely that the author of the font has decided to not allow modification and/or embedding of the font. This can happen quite often especially with "freeware" TrueType fonts. You must contact the author of the font for a less restricted version.</p>
+						
 						<p>Some files (especially old Mac PostScript fonts) require pre-processing that cannot be done on the server. For more information please read <a href="http://wiki.github.com/sorccu/cufon/trouble-with-font-files" class="external">Trouble with font files</a>.</p>
 						
 						<p class="info"><strong>Tip:</strong> did you upload an AFM or a PFM file? They only contain font metrics information which is not quite
