95b80a0a1d531ab43a6a1dcff1adcdfc00b5a75e
Simo Kinnunen <simo@shoqolate.com>
2009-07-02 23:52:45 +0300

    It is now possible to create fonts for 3rd-party scripts as well.

---
+++ generate/js/ui.js
@@ -0,0 +1,42 @@
+(function() {
+	
+	function addEvent(el, type, listener) {
+		if (el.addEventListener) {
+			el.addEventListener(type, listener, false);
+		}
+		else if (el.attachEvent) {
+			el.attachEvent('on' + type, function() {
+				return listener.call(el, window.event);
+			});
+		}
+	}
+	
+	function handle(el) {
+		switch (el.nodeName.toLowerCase()) {
+			case '#text':
+				// fall through
+			case 'img':
+				return handle(el.parentNode);
+			case 'a':
+				if (/callback/.test(el.className)) {
+					var callback = document.getElementById('icallback');
+					if (callback) {
+						callback.value = el.title;
+						el.blur();
+						return true;
+					}
+				}
+				return false;
+		}
+		return false;
+	}
+	
+	addEvent(document, 'click', function(e) {
+		if (!handle(e.target || e.srcElement)) return;
+		if (e.preventDefault) e.preventDefault();
+		else e.cancelBubble = true;
+		if (e.stopPropagation) e.stopPropagation();
+		else e.returnValue = false;
+	});
+	
+})();
