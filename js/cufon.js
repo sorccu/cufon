194ff60858ac32099f2e179ecbeb6df3622a764d
Simo Kinnunen <simo@shoqolate.com>
2009-09-11 09:20:37 -0400

    Added YUI (2.7) selector support.

--- js/cufon.js
+++ js/cufon.js
@@ -651,6 +651,7 @@ var Cufon = (function() {
 			||	(window.jQuery && function(query) { return jQuery(query); }) // avoid noConflict issues
 			||	(window.dojo && dojo.query)
 			||	(window.Ext && Ext.query)
+			||	(window.YAHOO && YAHOO.util && YAHOO.util.Selector && YAHOO.util.Selector.query)
 			||	(window.$$ && function(query) { return $$(query); })
 			||	(window.$ && function(query) { return $(query); })
 			||	(document.querySelectorAll && function(query) { return document.querySelectorAll(query); })
