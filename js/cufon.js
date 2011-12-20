8b19149d98ecc3deca50b92d57c722c83dcbebf3
Devolv <alfred.lee@gmail.com>
2011-12-20 09:38:15 -0500

    Update js/cufon.js

--- js/cufon.js
+++ js/cufon.js
@@ -1391,9 +1391,7 @@ Cufon.registerEngine('canvas', (function() {
 			canvas.width = canvasWidth * pixelRatio;
 			canvas.height = canvasHeight * pixelRatio;
 			g.scale(pixelRatio, pixelRatio);
-			
 			cStyle.top = Math.round((size.convert(expandTop - font.ascent)) / pixelRatio) + 'px';
-			cStyle.left = Math.round((size.convert(expandLeft)) / pixelRatio) + 'px';
 		}
 
 		// proper horizontal scaling is performed later
